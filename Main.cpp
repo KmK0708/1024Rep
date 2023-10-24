4e9d2ce379523b5b4095fccb1a1353a9929b06ad
KmK0708 <kample2k@gmail.com>
2023-10-24 17:55:48 +0900

    수업 추가 if문으로 p움직이기 ||와 && 으로 실행 출력

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
-{
-	cout << "Hello World!" << endl;
-	cout << "Git test commit!"<< endl;
+{	
+	bool A = false;
+	bool B = false;
+	bool C = true;
 
-	cout << "home test" << endl;
+	if (A && B != C)
+	{
+		cout << "실행" << endl;
+	}
 	return 0;
 }
+
+	//srand(time(nullptr));
+	//bool Decision = ((rand() % 2) == 0);
+
+	// if 문
+
+	//int PlayerX = 1;
+	//int PlayerY = 1;
+	//char PlayerShape ='P';
+	//bool bIsRunning = true;
+
+	//while (bIsRunning)
+	//{
+	//	int Key = _getch();
+	//	
+	//	system("cls");
+
+	//	if (Key == 'a'|| Key == 'A')
+	//	{
+	//		cout << "왼쪽" << endl;
+	//		PlayerX--;
+	//	}
+	//	else if (Key == 'w' || Key == 'W')
+	//	{
+	//		cout << "위" << endl;
+	//		PlayerY--;
+	//	}
+	//	else if (Key == 's' || Key == 'S')
+	//	{
+	//		cout << "아래" << endl;
+	//		PlayerY++;
+	//	}
+	//	else if (Key == 'd' || Key == 'D')
+	//	{
+	//		cout << "오른쪽" << endl;
+	//		PlayerX++;
+	//	}
+	//	else if (Key == 'q' || Key == 'Q')
+	//	{
+	//		cout << "종료" << endl;
+	//		bIsRunning = false;
+	//	}
+
+	//	cout << PlayerX << ", " << PlayerY << endl;
+
+	//	// x,y 좌표 설정
+	//	COORD pos = { PlayerX, PlayerY };
+	//	// 커서 이동
+	//	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	//	cout << PlayerShape;
+	//}
+
+	//return 0;
